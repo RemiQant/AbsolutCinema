98f0a24c2affc8610fc8a072d162cef98b97f262
Darrell Nicholas Tandean <darrelltandean@gmail.com>
2025-12-27 01:57:54 +0700

    feat (connecting fe to be): connected movie, seat, and showtime to use real data from neon's database rather than mockup data

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
+const BASE_URL = 'https://absolut-cinema-umwih.ondigitalocean.app/api';
 const api = axios.create({
-    baseURL: 'http://localhost:8080/api', 
+    baseURL: BASE_URL,
     withCredentials: true, 
     headers: {
         'Content-Type': 'application/json',
